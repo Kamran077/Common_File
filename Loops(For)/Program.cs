94481bea32b3c18d4afd7ec9aac559ff6d598ef8
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-05-22 22:52:21 +0400

    Add files via upload

---
+++ Loops(For)/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_For_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.ReadLine();
+            //for (int i = 0; i <= 10; i += 2)
+            //{
+            //    Console.WriteLine(i + " ");
+            //}
+
+            //for (int i = 1; i <= 10; i++)
+            //{ 
+            //    for(int j = 1; j<=i; j++)
+            //    {
+            //        Console.Write(j);
+            //    }
+            //    Console.WriteLine("");
+            //}
+            //Console.Write("Ededi daxil et : ");
+            //int eded = Convert.ToInt32(Console.ReadLine());
+
+            //string ededs = Convert.ToString(eded);
+
+            //for (int i = ededs.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(ededs[i] + " ");
+            //}
+
+            //int num = 0;
+
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    num += i;
+            //}
+            //Console.WriteLine(num);
+
+            Console.WriteLine("Num : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string fact = Convert.ToString(num);
+
+            for (int i = fact.Length; i > 0; i--)
+            {
+
+            }
+
+
+
+
+                Console.ReadLine();
+        }
+    }
+}
