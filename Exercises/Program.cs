882ed549f6ff174675e2a155d547970efb5831da
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-05-23 20:45:47 +0400

    Add files via upload

---
+++ Exercises/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 10, 20, 15, 15, 37, 20, 15, 50, 40, 40, 50, 60, 37, 40, 50, 55 };
+            int dene = 0;
+            int eded;
+
+
+
+            Console.Write("Ededi daxil et : ");
+            eded = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (eded == nums[i])
+                {
+                    dene++;
+                }
+
+            }
+
+            if (eded == 0)
+            {
+                Console.WriteLine("Daxil edilen eded siyahida yoxdur...");
+            }
+            else
+            {
+                Console.WriteLine("Daxil edilen eded siyahida var...");
+                Console.Write("Dene : " + dene);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
