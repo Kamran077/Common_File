7733d8a71a6549622d20fafedc76817359a33382
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-05-25 07:38:32 +0400

    Add files via upload

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int a = 1000;
+
+
+             byte b = (byte)a;
+
+             Console.WriteLine(b.GetType());
+             Console.WriteLine(b);
+             Console.ReadLine();*/
+
+            double x = 5.95;
+
+            int b = Convert.ToInt32(x);
+
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
