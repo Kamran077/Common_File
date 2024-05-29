0361c00f4e556f30f7015b2db860766cf214ebd7
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-05-29 18:28:31 +0400

    Add files via upload

---
+++ Foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+
+            /* for (int i = 0; i < nums.Length; i++)
+             {
+                 Console.WriteLine(nums[i]);
+             }*/
+
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
