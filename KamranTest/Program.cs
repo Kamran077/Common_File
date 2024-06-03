3b31d9c9f7c06dde67868afe61caec3a1afe454a
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-06-03 23:07:48 +0400

    Add files via upload

---
+++ KamranTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamranTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int total = 0 ;
+
+            for (int i = 0; i <= 10; i++)
+            {
+
+                total += i * i * i;
+
+
+               
+            }
+
+            Console.WriteLine(total);
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
